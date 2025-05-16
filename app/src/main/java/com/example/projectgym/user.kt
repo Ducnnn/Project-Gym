5b3c6e628bf62988503deba7e372863c9d59625e
Densilz <dinault08@gmail.com>
2025-05-16 15:41:36 -0400

    i created classes for user,meals,and training days

---
+++ app/src/main/java/com/example/projectgym/user.kt
@@ -0,0 +1,57 @@
+package com.example.projectgym
+
+class User (
+    var name: String,
+    var age: Float,
+    var height: Float,
+    var weight: Float,
+    var gender: String,
+    var program: MutableList<TranDay>
+)
+
+class TranDay (
+    var name: String,
+    var exercises: MutableList<Exercise> = mutableListOf<Exercise>(),
+) {
+    fun addExercise(exercise : Exercise) {
+        exercises.add (exercise)
+    }
+
+}
+
+class Exercise (
+    var name: String,
+    var sets: Int,
+    var muscle: String
+)
+
+class CurDay (
+    // var trainingDay
+    // val date
+)
+
+class ListMeals (
+    var meals: MutableList<Meals> = mutableListOf()
+) {
+    fun addMeal(meal: Meals) {
+        meals.add (meal)
+    }
+}
+
+class Meals (
+    var name: String,
+    var products: MutableList<Products> = mutableListOf()
+) {
+    fun addProducts(product: Products) {
+        products.add(product)
+    }
+}
+
+class Products (
+    var name: String,
+    var calories: Float,
+    var proteins: Float,
+    var fats: Float,
+    var carbs: Float
+)
+
